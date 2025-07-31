adbac388249ca4b4deae6834199fa998cff4c2c9
Hasan Khalil <mian.hasan.khalil@gmail.com>
2025-07-31 02:00:43 -0400

    Disable InfluxDB reporting by default.

--- src/config.example.h
+++ src/config.example.h
@@ -10,7 +10,9 @@
 #define IR_TX_PIN 0
 
 // InfluxDB stats reporting.
+/*
 const IPAddress kInfluxHost(192,168,8,1);  // Influx DB Host
 constexpr uint16_t kInfluxPort=8096;  // Influx DB UDP Port
 // InfluxDB sample push interval.
 #define PUSH_SAMPLE_INTERVAL_IN_S 10
+*/
