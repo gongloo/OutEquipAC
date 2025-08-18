d3d827f9a6709cfa10d5ce444fcc165d3f6ea592
Hasan Khalil <mian.hasan.khalil@gmail.com>
2025-08-18 10:58:51 -0400

    UI changes only.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.3.44"
+        #define VERSION "0.3.45"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-08-02 02:20:26.776472"
+        #define BUILD_TIMESTAMP "2025-08-11 07:02:53.868855"
     #endif
     
