2a982bfac44c73530822570f5cf8efc4d97e7a69
Hasan Khalil <mian.hasan.khalil@gmail.com>
2025-09-02 00:44:42 -0400

    Minor UI improvements: iOS chrome color.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.3.51"
+        #define VERSION "0.3.52"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-08-31 23:47:38.356975"
+        #define BUILD_TIMESTAMP "2025-09-01 23:57:20.990561"
     #endif
     
